b573dc39ae984b2c0ae1d94a308a02849f748edd
Eduardo <edu...@gmail.com>
2013-03-19 10:24:31 +0100

    Ejercico 3 ok y sol

---
+++ Ejercicio 3/Ejercicio 3/build/generated-sources/ap-source-output/curso/ejercicio3/db/Task_.java
@@ -0,0 +1,15 @@
+package curso.ejercicio3.db;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-03-15T12:33:49")
+@StaticMetamodel(Task.class)
+public class Task_ { 
+
+    public static volatile SingularAttribute<Task, Integer> id;
+    public static volatile SingularAttribute<Task, String> text;
+    public static volatile SingularAttribute<Task, Boolean> done;
+
+}
